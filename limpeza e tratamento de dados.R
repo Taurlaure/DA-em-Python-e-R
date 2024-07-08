3c8ed32a0b5ab751d6d696448e709112b3c98c7f
Gustavo Amorim <gustavo.gpa@hotmail.com>
2024-07-08 11:15:28 -0300

    Coninuacao de tratamento e limpeza de dados em R

--- limpeza e tratamento de dados.R
+++ limpeza e tratamento de dados.R
@@ -12,7 +12,7 @@ summary(dados)
 colnames(dados) = c("ID","SCORE","ESTADO","GENERO","IDADE","PATRIMONIO","SALDO","PRODUTOS","TEMCARTCREDITO","ATIVO","SALARIO","SAIU")
 head(dados)
 
-#Explorar dados , colunas categÃ³ricas
+#Explorar dados , colunas categóricas
 
 #Estados
 
@@ -29,3 +29,63 @@ barplot(countsGenero, main="GENEROS" , xlab = "GENEROS")
 summary(dados$SCORE)
 boxplot(dados$SCORE)
 hist(dados$SCORE)
+
+#Idade
+summary(dados$IDADE)
+boxplot(dados$IDADE)
+hist(dados$IDADE)
+
+#Saldo
+summary(dados$SALDO)
+boxplot(dados$SALDO)
+hist(dados$SALDO)
+
+#Salário
+summary(dados$SALARIO)
+boxplot(dados$SALARIO)
+boxplot(dados$SALARIO,outline = F)
+
+#Valores faltantes em NA'S
+
+dados[!complete.cases(dados),]
+
+#Salarios
+
+#tratar salarios
+summary(dados$SALARIO)
+#ver mediana
+median(dados$SALARIO, na.rm = T)
+#atribuir mediana nos NAs
+dados[is.na(dados$SALARIO),]$SALARIO = median(dados$SALARIO,na.rm = T)
+#buscar NAS em salario para checar
+dados[!complete.cases(dados$SALARIO),]
+
+#Falta de padronização de genero
+
+#ver valores
+unique(dados$GENERO)
+summary(dados$GENERO)
+#transformar F e Fem em Feminino
+#"", M em Masculino (moda)
+dados[is.na(dados$GENERO) | dados$GENERO == "M",]$GENERO = "Masculino"
+dados[dados$GENERO == "F" | dados$GENERO == "Fem",]$GENERO = "Feminino"
+#ver resultados
+summary(dados$GENERO)
+#remover levels nao utilizados
+dados$GENERO = factor(dados$GENERO)
+#visualizar novamente
+summary(dados$GENERO)
+
+#Idades fora do dominio
+
+summary(dados$IDADE)
+#fora do dominio numerico , ver idades anormais
+dados[dados$IDADE<0 | dados$IDADE>110,]$IDADE
+#não temos idade NAs
+dados[is.na(dados$IDADE),]
+#opção , preencher com mediana
+median(dados$IDADE)
+#substituição
+dados[dados$IDADE<0 | dados$IDADE>110,]$IDADE = median(dados$IDADE)
+dados[dados$IDADE<0 | dados$IDADE>110,]
+summary(dados$IDADE)
